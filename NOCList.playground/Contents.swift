dbc073ddca8a889c1eab77659bbb2bf563fba44d
nuvallo <nuvallo.inc@gmail.com>
2020-11-04 17:50:45 -0500

    steps 9 & 10 completed

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -109,12 +109,29 @@ findHighRisk(arr: arr)
 
 //: ## Step 9
 //: Create a function that finds totals for low, mid, and high level agents. Low level agents are 4 or lower, mid are 5-7, and high level agents are 8 or above. Iterate over each agent and use a `switch` statement to determine their level group. At the end of the function, print a statement like the following: "# low level agents, # mid level agents, and # high level agents"
+func findsTotal(arr: Array<(coverName: String, realName: String, accessLevel: Int, compromised: Bool)>) {
+    var lowLevel: Int = 0
+    var midLevel: Int = 0
+    var highLevel: Int = 0
+    for i in arr {
+        switch i.accessLevel {
+        case 1,2,3,4:
+            lowLevel += 1
 
+        case 5,6,7:
+            midLevel += 1
+
+        default:
+            highLevel += 1
+        }
+    }
+    print("\(lowLevel) low level agents, \(midLevel) mid level agents, and \(highLevel) high level agents")
+}
 
 
 //: ## Step 10
 //: Call the above function and check its output in the console.
-
+findsTotal(arr: arr)
 
 
 //: ## Step 11 (Optional)
